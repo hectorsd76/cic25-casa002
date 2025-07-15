0820b763c277e6b531c26accb5ab25ec40bfe850
hsolana <hsolana@formacion.cic.es>
2025-07-15 20:50:17 +0200

    Inicializo proyecto de spring

---
+++ src/main/java/es/cic/curso2025/casa002/Casa002Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso2025.casa002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Casa002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Casa002Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso2025/casa002/Casa002ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso2025.casa002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casa002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
